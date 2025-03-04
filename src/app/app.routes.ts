d957a62083efe237c8f4a695f60503c98e58e6eb
AndersonSilvaJob <andersonsilva.job01@gmail.com>
2025-03-04 11:48:23 -0300

    Integrando com o BackEnd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
+import { ClientComponent } from './pages/client/client.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [{
     path: "login",
@@ -9,5 +11,10 @@ export const routes: Routes = [{
 {
     path: "signup",
     component: SignUpComponent
+},
+{
+    path: "client",
+    component: ClientComponent,
+    canActivate: [AuthGuard]
 }
 ];
