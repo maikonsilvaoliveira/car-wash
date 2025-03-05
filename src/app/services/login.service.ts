9fe9ffcd3a23d02e2c2b15448d6f2fffc523ff26
AndersonSilvaJob <andersonsilva.job01@gmail.com>
2025-03-05 10:27:45 -0300

    Adcionando basic redirect

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../types/login-response.type';
 import { tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,14 @@ export class LoginService {
 
   apiUrl: string = "http://localhost:8080/auth";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
   login(email: string , password: string){
     return this.httpClient.post<LoginResponse>(this.apiUrl + '/login',{email, password}).pipe(
       tap((value)=>{
         sessionStorage.setItem("auth-token",value.token)
         sessionStorage.setItem("username",value.name)
+        this.router.navigate(['/client']);
       })
     )
   }
