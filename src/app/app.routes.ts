1f54a0791996f4f1c4b603d8614f4d565662acb5
AndersonSilvaJob <andersonsilva.job01@gmail.com>
2025-03-07 17:14:45 -0300

    Adicionando menu e dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,15 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
 import { ClientComponent } from './pages/client/client.component';
 import { AuthGuard } from './services/auth-guard.service';
+import { MenuComponent } from './pages/menu/menu.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [{
-    path: "login",
-    component: LoginComponent
-},
-{
-    path: "signup",
-    component: SignUpComponent
-},
-{
-    path: "client",
-    component: ClientComponent,
-    canActivate: [AuthGuard]
-}
+export const routes: Routes = [
+  { path: "login", component: LoginComponent},
+  { path: "signup",component: SignUpComponent},
+  { path: "client",component: ClientComponent, canActivate: [AuthGuard]},
+  { path: "menu", component: MenuComponent,
+      children: [
+      { path: "dashboard", component: DashboardComponent }] // Dashboard carregado dentro do Menu
+  }
 ];
